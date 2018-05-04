10f9dd8c51c32c55753da2afdbc694a1bd589c0f
Dmitriy Velychko <velychko.dmitriy@gmail.com>
2018-05-04 18:38:14 +0300

    Created Login page

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -1,7 +1,9 @@
 import * as firebase from 'firebase';
 import 'firebase/database';
+import 'firebase/auth';
 
 import { firebaseConfig } from './config';
 
 export const firebaseApp = firebase.initializeApp(firebaseConfig);
 export const firebaseDb = firebase.database();
+export const firebaseAuth = firebase.auth();
