c59dc9095e37673e7d3129dd739e6ed27a69a6dd
Dmitriy Velychko <velychko.dmitriy@gmail.com>
2018-05-11 18:02:50 +0300

    Added different providers to app

--- src/auth/Login.tsx
+++ src/auth/Login.tsx
@@ -1,26 +1,76 @@
 import * as React from 'react';
-import { Button } from 'antd';
+import { Button, Input, Icon } from 'antd';
 import * as firebase from 'firebase';
 
 import './Login.css';
+
+import { App } from '../main';
 
 import { firebaseAuth } from '../firebase';
 
 interface State {
   user: firebase.User | null;
+  login: {
+    email: string;
+    password: string;
+  };
 }
 
 export class Login extends React.PureComponent<{}, State> {
   public state = {
     user: null,
+    login: {
+      email: '',
+      password: '',
+    },
   };
 
   public render() {
+    const { user } = this.state;
+
+    console.log(user);
+    firebaseAuth.signOut();
+    if (user) {
+      return <App user={user} />;
+    }
+
     return (
       <div className="Login">
-        <Button className="Login-google" size="large" onClick={this.signIn}>
-          Google Sign
-        </Button>
+        <h1>Log In</h1>
+        <div className="Login-auth">
+          <Input
+            className="Login-input"
+            size="large"
+            prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+            placeholder="Enter email"
+          />
+          <Input
+            className="Login-input"
+            size="large"
+            prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+            placeholder="Enter password"
+          />
+          <Button
+            type="primary"
+            className="Login-google"
+            size="large"
+            onClick={this.signUpWithEmail}
+          >
+            Sign In
+          </Button>
+        </div>
+
+        <div className="Login-others">
+          <Button size="large" onClick={this.signInWithGoogle}>
+            Google
+          </Button>
+          <Button size="large" onClick={this.signInWithFacebook}>
+            Facebook
+          </Button>
+          <Button size="large" onClick={this.signInWithGithub}>
+            Github
+          </Button>
+        </div>
       </div>
     );
   }
@@ -29,15 +79,31 @@ export class Login extends React.PureComponent<{}, State> {
     firebaseAuth.onAuthStateChanged((user: firebase.User) => {
       if (user) {
         this.setState({ user });
-        // TODO: Redirect
-      } else {
-        // TODO: Redirect Login
       }
     });
   }
 
-  private signIn = () => {
+  private signUpWithEmail = () => {
+    const { login: { email, password } } = this.state;
+    firebaseAuth.createUserWithEmailAndPassword(email, password).catch(error => {
+      if (error.code) {
+        firebaseAuth.signInWithEmailAndPassword(email, password);
+      }
+    });
+  };
+
+  private signInWithFacebook = () => {
+    const provider = new firebase.auth.FacebookAuthProvider();
+    firebaseAuth.signInWithPopup(provider);
+  };
+
+  private signInWithGoogle = () => {
     const provider = new firebase.auth.GoogleAuthProvider();
+    firebaseAuth.signInWithPopup(provider);
+  };
+
+  private signInWithGithub = () => {
+    const provider = new firebase.auth.GithubAuthProvider();
     firebaseAuth.signInWithPopup(provider);
   };
 }
